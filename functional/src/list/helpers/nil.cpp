3661d340cfd6d7fc0999eaabd1b5e0adc5165598
Evan Pacini <evan.pacini@gmail.com>
2023-10-27 02:20:00 +0200

    Move nil.cpp to the helpers folder

--- functional/src/list/helpers/nil.cpp
+++ functional/src/list/helpers/nil.cpp
@@ -1,4 +1,4 @@
-#include <types/list/list_double.h>
+#include "types/list/list_double.h"
 
 ListDoubleStruct NilStruct = {.head = __builtin_bit_cast(double, nilBits), .tail = nullptr, .size = 0};
 extern "C" ListDouble NilDouble = &NilStruct;
